3146487cad43066d01c6d0886cdb370386687cc8
David Schneider <david.jay.schneider@gmail.com>
2020-01-07 15:49:21 -0500

    adds changes to express templating

--- album-browser/index.js
+++ album-browser/index.js
@@ -3,8 +3,12 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-
 const album = require('./album');
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
@@ -13,17 +17,34 @@ app.get('/', (req, res) => {
 })
 
 app.get('/albums', (req, res) => {
+    const albumData = album.getAlbums();
+    // console.log(albumData);
+    const albumHTML = albumData.map((album) => {
+        return `<h2><a href='/albums/${album.id}'>${album.artist}: ${album.title}</a></h2>`
+    }).join('');
+    // console.log(albumHTML);
     try {
-        const albumInfo = album.getAlbums();
-        for (let album of albumInfo) {
-            res.write(`
-            <p>
-                <a href="/albums/${album.id}">
-                    ${album.title} : ${album.artist}
-                </a>
-            </p>`);
-        }
-        res.end();
+        res.render('albums', {
+            locals: {
+                pageTitle: 'Album List',
+                albums: albumHTML
+            },
+            partials: {
+                header: 'partials/header',
+                pageHeader: 'partials/pageHeader',
+                footer: 'partials/footer'
+            }
+        })
+        // const albumInfo = album.getAlbums();
+        // for (let album of albumInfo) {
+        //     res.write(`
+        //     <p>
+        //         <a href="/albums/${album.id}">
+        //             ${album.title} : ${album.artist}
+        //         </a>
+        //     </p>`);
+        // }
+        // res.end();
     } catch (e) {
         res.status(404);
         res.send('Could not find albums list.');
@@ -33,17 +54,38 @@ app.get('/albums', (req, res) => {
 app.get('/albums/:albumId', (req, res) => {
     try {
         const albumSongs = album.getSongsForAlbum(req.params.albumId);
-        for (let song of albumSongs) {
-            res.write(`
-            <p>
-                ${song.title}
-            </p>`);
-        }
-        res.end();
+        // console.log(albumSongs);
+        const songsHTML = albumSongs.map((song) => {
+            return `<p>${song.title}</p>`;
+        }).join('');
+        res.render('songs', {
+            locals: {
+                pageTitle: 'Songs on Album:',
+                songList: songsHTML
+            },
+            partials: {
+                header: 'partials/header',
+                pageHeader: 'partials/pageHeader',
+                footer: 'partials/footer'
+            }
+        })
     } catch (e) {
         res.status(404);
-        res.send('Album not found.');
+        res.end('Data not found.');
     }
+    // try {
+    //     const albumSongs = album.getSongsForAlbum(req.params.albumId);
+    //     for (let song of albumSongs) {
+    //         res.write(`
+    //         <p>
+    //             ${song.title}
+    //         </p>`);
+    //     }
+    //     res.end();
+    // } catch (e) {
+    //     res.status(404);
+    //     res.send('Album not found.');
+    // }
 });
 
 app.get('*', (req, res) => {
