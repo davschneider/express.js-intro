2786c7841cb912aaab715e21752ed79eaa9fce83
David Schneider <david.jay.schneider@gmail.com>
2020-01-07 14:48:15 -0500

    adds changes to partials and templates

--- html-album-browser/index.js
+++ html-album-browser/index.js
@@ -10,6 +10,22 @@ app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
+const blogData = [
+    {
+        title: 'first blog post',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sint, excepturi soluta impedit qui laboriosam aperiam iusto recusandae quos autem enim asperiores nulla ad tempore aspernatur suscipit, illum quod dolorem!'
+    },
+    {
+        title: 'second blog post',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sint, excepturi soluta impedit qui laboriosam aperiam iusto recusandae quos autem enim asperiores nulla ad tempore aspernatur suscipit, illum quod dolorem!'
+    },
+    {
+        title: 'third blog post',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sint, excepturi soluta impedit qui laboriosam aperiam iusto recusandae quos autem enim asperiores nulla ad tempore aspernatur suscipit, illum quod dolorem!'
+    }
+];
+
+
 app.get('/', (req, res) => {
     res.render('home', {
         locals: {
@@ -23,6 +39,46 @@ app.get('/', (req, res) => {
             footer: 'partials/footer'
         }
     });
+});
+
+app.get('/blog', (req, res) => {
+    // const blogHTML = [];
+    // for (let post of blogData) {
+    //     blogHTML.push(`<h2>${post.title}</h2>`);
+    //     blogHTML.push(`<p>${post.content}</p>`);
+    // }
+    const blogHTML = blogData.map((post) => {
+        return `<h2>${post.title}</h2><p>${post.content}</p>`
+    });
+    console.log(blogHTML);
+
+    res.render('blog', {
+        locals: {
+            blogPosts: blogData,
+            pageTitle: 'Blog',
+            pageHeader: 'Blog'
+        },
+        partials: {
+            header: 'partials/header',
+            nav: 'partials/nav',
+            footer: 'partials/footer'
+        }
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('home', {
+        locals: {
+            pageTitle: 'About',
+            pageHeader: 'About'
+        },
+        partials: {
+            header: 'partials/header',
+            nav: 'partials/nav',
+            title: 'partials/title',
+            footer: 'partials/footer'
+        }
+    })
 });
 
 server.listen(PORT, () => {
